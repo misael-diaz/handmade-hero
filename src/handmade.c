689f455ab2724736e65fb6caa9c5c325bffa963f
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-03-03 15:19:16 -0400

    adds initial window code

--- src/handmade.c
+++ src/handmade.c
@@ -1,16 +1,79 @@
 #include <windows.h>
 
-int CALLBACK WinMain(
-		HINSTANCE hInstance,
-		HINSTANCE hPrevInstance,
-		LPSTR lpCmdLine,
-		int nCmdShow)
+LRESULT CALLBACK MainWindowCallback(
+		HWND Window,
+		UINT Message,
+		WPARAM WParam,
+		LPARAM LParam)
 {
-	MessageBox(
+	LRESULT Result = {};
+	switch(Message) {
+		case WM_SIZE:
+		{
+			OutputDebugString("WM_SIZE");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugString("WM_DESTROY");
+		} break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugString("WM_CLOSE");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugString("WM_ACTIVEAPP");
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+			//OutputDebugString("default");
+		} break;
+	}
+}
+
+int CALLBACK WinMain(
+		HINSTANCE Instance,
+		HINSTANCE PrevInstance,
+		LPSTR CommandLine,
+		int ShowCode)
+{
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "HandMadeHeroWindowClass";
+	if (RegisterClass(&WindowClass)) {
+		HWND Window = CreateWindowEx(
 			0,
-			"This is Handmade Hero",
+			WindowClass.lpszClassName,
 			"Handmade Hero",
-			MB_OK | MB_ICONINFORMATION
-		  );
+			WS_EX_OVERLAPPEDWINDOW | WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+			0
+		);
+		if (WindowHandle) {
+			MSG Message = {};
+			for (;;) {
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (0 < MessageResult) {
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				} else {
+					break;
+				}
+			}
+		}
+	}
 	return 0;
 }
