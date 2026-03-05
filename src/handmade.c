76bedc4fceb3b53c79641e62c097a3e38ee2cb4a
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-03-05 15:13:29 -0400

    renders bitmap to window

--- src/handmade.c
+++ src/handmade.c
@@ -1,6 +1,97 @@
+#include <stdint.h>
 #include <windows.h>
 
-LRESULT CALLBACK MainWindowCallback(
+#define global static
+#define local_persist static
+#define internal static
+#define BITMAP_DEPTH 32
+
+typedef uint8_t uint8
+typedef uint32_t uint32
+
+// TODO this is global for now
+
+global bool running;
+global BITMAPINFO BitmapInfo;
+global void *Bitmap;
+global int BitmapWidth;
+global int BitmapHeight;
+
+// DIB Device Independent Bitmap
+// Thanks to Chris Hecker of Spy party fame for his insightful comments (I'm keeping the
+// thank you note as Casey did on the stream day 4 session).
+internal void Win32ResizeDIBSection(int const Width, int const Height)
+{
+	BitmapWidth = Width;
+	BitmapHeight = Height;
+	// NOTE we want a bitmamp with a top-left origin so by convention biHeight < 0
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+	BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = BITMAP_DEPTH;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	if (Bitmap) {
+		VirtualFree(Bitmap, 0, MEM_RELEASE);
+		Bitmap = NULL;
+	}
+	int const BitmapSize = (BITMAP_DEPTH / 8) * (BitmapWidth * BitmapHeight);
+	DWORD const AllocationType = MEM_COMMIT | MEM_RESERVE;
+	Bitmap = VirtualAlloc(0, BitmapSize, AllocationType, PAGE_READWRITE);
+	uint8 *Row = Bitmap;
+	uint32 *Pixel = Bitmap;
+	size_t const Pitch = (BITMAP_DEPTH / 8) * BitmapWidth;
+	for(int y = 0; y != BitmapHeight; ++y) {
+		Pixel = (uint32*) Row;
+		uint8 const R = 0xff; // Red
+		uint8 const G = 0xff; // Green
+		uint8 const B = 0xff; // Blue
+		uint8 const P = 0x00; // Pad
+		for(int x = 0; x != BitmapWidth; ++x) {
+			*(Pixel + x) = ((P << 24) + (R << 16) + (G << 8) + (B << 0));
+		}
+		Row += Pitch;
+	}
+}
+
+internal void Win32UpdateWindow(
+		HDC DeviceContext,
+		RECT const * const WindowRect,
+		int const X,
+		int const Y,
+		int const Width,
+		int const Height)
+{
+	int const WindowWidth = WindowRect->right - WindowRect->left;
+	int const WindowHeight = WindowRect->bottom - WindowRect->top;
+	StretchDIBits(
+		DeviceContext,
+/*
+		X,
+		Y,
+		Width,
+		Height,
+		X,
+		Y,
+		Width,
+		Height,
+*/
+		0,
+		0,
+		BitmapWidth,
+		BitmapHeight,
+		0,
+		0,
+		WindowWidth,
+		WindowHeight,
+		Bitmap,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY
+	);
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(
 		HWND Window,
 		UINT Message,
 		WPARAM WParam,
@@ -10,17 +101,28 @@ LRESULT CALLBACK MainWindowCallback(
 	switch(Message) {
 		case WM_SIZE:
 		{
+			RECT ClientRect = {};
+			GetClientRect(Window, &ClientRect);
+			int const Width = (
+				ClientRect.right - ClientRect.rcClientRect.left
+			);
+			int const Height = (
+				ClientRect.bottom - ClientRect.rcClientRect.top
+			);
+			Win32ResizeDIBSection(Width, Height);
 			OutputDebugString("WM_SIZE");
 		} break;
 
 		case WM_DESTROY:
 		{
-			OutputDebugString("WM_DESTROY");
+			// TODO treat this as an unexpected error and restore window?
+			running = false;
 		} break;
 
 		case WM_CLOSE:
 		{
-			OutputDebugString("WM_CLOSE");
+			// TODO should display message to the player first?
+			running = false;
 		} break;
 
 		case WM_ACTIVATEAPP:
@@ -37,7 +139,15 @@ LRESULT CALLBACK MainWindowCallback(
 			int const Y = Paint.rcPaint.top;
 			int const Width = (Paint.rcPaint.right - Paint.rcPaint.left);
 			int const Height = (Paint.rcPaint.bottom - Paint.rcPaint.top);
-			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+			RECT WindowRect = {};
+			GetWindowRect(Window, &WindowRect);
+			Win32UpdateWindow(
+				DeviceContext,
+				&WindowRect,
+				X,
+				Y,
+				Width,
+				Height);
 			EndPaint(Window, &Paint);
 		} break;
 
@@ -57,7 +167,7 @@ int CALLBACK WinMain(
 {
 	WNDCLASS WindowClass = {};
 	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.lpfnWndProc = Win32MainWindowCallback;
 	WindowClass.hInstance = Instance;
 	WindowClass.lpszClassName = "HandMadeHeroWindowClass";
 	if (RegisterClass(&WindowClass)) {
@@ -77,7 +187,8 @@ int CALLBACK WinMain(
 		);
 		if (WindowHandle) {
 			MSG Message = {};
-			for (;;) {
+			running = true;
+			while (running) {
 				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
 				if (0 < MessageResult) {
 					TranslateMessage(&Message);
