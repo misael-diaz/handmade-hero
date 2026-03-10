22b4702bb0b66af02c4912787dae29d2cb0b8bed
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-03-10 18:59:29 -0400

    gets handle to xlib window

--- src/linux_handmade.c
+++ src/linux_handmade.c
@@ -10,6 +10,38 @@ int main()
 		fprintf(stdout, "%s", "XErrorOpenDisplay\n");
 		exit(EXIT_FAILURE);
 	}
+	Window root = DefaultRootWindow(display);
+	Screen *screen = DefaultScreenOfDisplay(display);
+	int screeno = DefaultScreen(display);
+	XSetWindowAttributes attributes = {};
+	attributes.background_pixel = BlackPixel(display, screeno);
+	attributes.event_mask = (
+		ExposureMask |
+		KeyPressMask |
+		KeyReleaseMask
+	);
+
+	int const x = 0;
+	int const y = 0;
+	int const width = WidthOfScreen(screen);
+	int const height = HeightOfScreen(screen);
+	int const window_border_width = 0;
+	int const screen_depth = DefaultDepthOfScreen(screen);
+	Visual *visual = DefaultVisual(display, screeno);
+	Window window = XCreateWindow(
+		display,
+		root,
+		x,
+		y,
+		width,
+		height,
+		window_border_width,
+		screen_depth,
+		InputOutput,
+		visual,
+		CWBackPixel | CWEventMask,
+		&attributes
+	);
 
 	XCloseDisplay(display);
 	return 0;
