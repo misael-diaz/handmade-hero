316d9e28b6c28eed1380007e3a51e00cd78e8da6
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-03-14 17:01:48 -0400

    renders a green screen with Xlib
    NOTES
    Quake II's source code was a great reference for me to implement this.
    
    I must admit that the code is probably not going to work if the X server
    expects the data in BE; however most desktops and laptops use LE
    so we should not worry too much the colors might not look right.
    However our code will bail out if it encounters BE layout so we
    will know if we ever have to support this.
    
    Note that we are writting to an int so each pixel is 32-bits deep
    RGB + padding. Also there are zero bytes between scanlines and that's
    why we pass zero to XPutImage. I think this covers the magic numbers
    that could be puzzling at first.
    
    At this point main has started to grow so that we shall consider
    in the near future refactoring.
    
    Later we can display the gradient that Casey uses in the first few
    days of development.

--- src/linux/linux_handmade.c
+++ src/linux/linux_handmade.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include <stdbool.h>
 #include <string.h>
 #include <sys/mman.h>
@@ -67,7 +68,53 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	// NOTE: here we are assuming data to be layout in LE
+	if (LSBFirst != ImageByteOrder(display)) {
+		fprintf(stderr, "%s", "Fatal Unsupported Display Error\n");
+		XCloseDisplay(display);
+		display = NULL;
+		exit(EXIT_FAILURE);
+	}
+
+	int nvisuals = 0;
+	XVisualInfo template = {};
+	template.visualid = XVisualIDFromVisual(visual);
+	XVisualInfo *visinfo = XGetVisualInfo(
+		display,
+		VisualIDMask,
+		&template,
+		&nvisuals
+	);
+
+	fprintf(stdout, "bitmap-pad %d\n", BitmapPad(display));
+	fprintf(stdout, "depth %d\n", visinfo->depth);
+	fprintf(stdout, "red-mask 0x%lx\n", visual->red_mask);
+	fprintf(stdout, "green-mask 0x%lx\n", visual->green_mask);
+	fprintf(stdout, "blue-mask 0x%lx\n", visual->blue_mask);
+
+	long unsigned red_shift = 0;
+	long unsigned green_shift = 0;
+	long unsigned blue_shift = 0;
+
+	const long unsigned rgb_mask = 0xff;
+	while ((rgb_mask << red_shift) != visual->red_mask) {
+		red_shift += 8LU;
+	}
+
+	while ((rgb_mask << green_shift) != visual->green_mask) {
+		green_shift += 8LU;
+	}
+
+	while ((rgb_mask << blue_shift) != visual->blue_mask) {
+		blue_shift += 8LU;
+	}
+
+	fprintf(stdout, "red-shift %ld\n", red_shift);
+	fprintf(stdout, "green-shift %ld\n", green_shift);
+	fprintf(stdout, "blue-shift %ld\n", blue_shift);
+
 	GC gc = DefaultGC(display, screeno);
+
 	XEvent ev = {};
 	XStoreName(display, window, "Handmade Hero");
 	XMapWindow(display, window);
@@ -91,6 +138,7 @@ int main()
 		if (errno) {
 			fprintf(stderr, "%s\n", strerror(errno));
 		}
+		XFree(visinfo);
 		XDestroyWindow(display, window);
 		XCloseDisplay(display);
 		display = NULL;
@@ -98,6 +146,34 @@ int main()
 	}
 
 	Memory.TransientStorage = Memory.PermanentStorage + Memory.PermanentStorageSize;
+
+	uint8_t const red = 0;
+	uint8_t const green = 0xff;
+	uint8_t const blue = 0;
+	size_t const pixels = width * height;
+	size_t const framesz = pixels * 4;
+	int *framebuffer = Memory.TransientStorage;
+	for (long unsigned i = 0; i != pixels; ++i) {
+		framebuffer[i] = (
+			(red << red_shift) + (green << green_shift) + (blue << blue_shift)
+		);
+	}
+
+	char *data = Memory.TransientStorage;
+	XImage *image = XCreateImage(
+		display,
+		visual,
+		visinfo->depth,
+		ZPixmap,
+		0,
+		data,
+		width,
+		height,
+		32,
+		0
+	);
+
+	XPutImage(display, window, gc, image, 0, 0, 0, 0, width, height);
 
 	// TODO: allocate the bitmap
 	struct game_offscreen_buffer Buffer = {};
@@ -110,6 +186,14 @@ int main()
 	fprintf(stdout, "%s", "press any key to exit the game\n");
 	fread(&c, sizeof(c), 1, stdin);
 
+	// NOTE: we have to nullify because Xlib will try to free the data but this is
+	//       going to cause a problem because the data is not heap allocated so the
+	//       right thing here is to nullify so that Xlib won't attempt to free a
+	//       memory region that has not been allocated. We have checked this with
+	//       valgrind.
+	image->data = NULL;
+	XDestroyImage(image);
+	XFree(visinfo);
 	XDestroyWindow(display, window);
 	XCloseDisplay(display);
 	return 0;
