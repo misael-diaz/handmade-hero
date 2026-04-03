5d3629d7f0912aac8e1bdbe1cc68e96aad7fd4b2
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2026-04-03 09:29:06 -0400

    passes real32 coords for DrawRectangle
    NOTES
    following Casey on this one for reasons that will become crystal
    clear further down the series

--- src/common/handmade.c
+++ src/common/handmade.c
@@ -1,9 +1,17 @@
 #include "handmade.h"
 
-internal int Clamp(
-	int const xval,
-	int const xmin,
-	int const xmax
+internal inline uint32 RoundReal32ToUInt32(
+	real32 value
+) {
+	Assert(0.0f <= value);
+	value += 0.5f;
+	return ((uint32) value);
+}
+
+internal inline real32 Clamp(
+	real32 const xval,
+	real32 const xmin,
+	real32 const xmax
 ) {
 	if (xmin > xval) {
 		return xmin;
@@ -14,55 +22,56 @@ internal int Clamp(
 	}
 }
 
+// TODO: extend to set the rectangle color
 internal void DrawRectangle(
 	struct game_memory * const Memory,
-	int xmin,
-	int xmax,
-	int ymin,
-	int ymax
+	real32 xmin,
+	real32 xmax,
+	real32 ymin,
+	real32 ymax
 ) {
 	struct game_state *GameState = Memory->PermanentStorage;
 	char unsigned *framebuffer = Memory->TransientStorage;
 	Assert(GameState);
 	Assert(framebuffer);
 
-	xmin = Clamp(xmin, 0, HH_GAME_WINDOW_WIDTH);
-	xmax = Clamp(xmax, 0, HH_GAME_WINDOW_WIDTH);
-	ymin = Clamp(ymin, 0, HH_GAME_WINDOW_HEIGHT);
-	ymax = Clamp(ymax, 0, HH_GAME_WINDOW_HEIGHT);
+	xmin = Clamp(xmin, 0.0f, (real32) HH_GAME_WINDOW_WIDTH);
+	xmax = Clamp(xmax, 0.0f, (real32) HH_GAME_WINDOW_WIDTH);
+	ymin = Clamp(ymin, 0.0f, (real32) HH_GAME_WINDOW_HEIGHT);
+	ymax = Clamp(ymax, 0.0f, (real32) HH_GAME_WINDOW_HEIGHT);
 
 	if (xmin > xmax) {
-		int const temp = xmax;
+		real32 const temp = xmax;
 		xmax = xmin;
 		xmin = temp;
 	}
 
 	if (ymin > ymax) {
-		int const temp = ymax;
+		real32 const temp = ymax;
 		ymax = ymin;
 		ymin = temp;
 	}
 
 	Assert(xmin < xmax);
 	Assert(ymin < ymax);
-	Assert(0 <= xmin);
-	Assert(0 <= xmax);
-	Assert(0 <= ymin);
-	Assert(0 <= ymax);
-	Assert(HH_GAME_WINDOW_WIDTH  >= xmin);
-	Assert(HH_GAME_WINDOW_WIDTH  >= xmax);
-	Assert(HH_GAME_WINDOW_HEIGHT >= ymin);
-	Assert(HH_GAME_WINDOW_HEIGHT >= ymax);
+	Assert(0.0f <= xmin);
+	Assert(0.0f <= xmax);
+	Assert(0.0f <= ymin);
+	Assert(0.0f <= ymax);
+	Assert(((real32) HH_GAME_WINDOW_WIDTH ) >= xmin);
+	Assert(((real32) HH_GAME_WINDOW_WIDTH ) >= xmax);
+	Assert(((real32) HH_GAME_WINDOW_HEIGHT) >= ymin);
+	Assert(((real32) HH_GAME_WINDOW_HEIGHT) >= ymax);
 
-	size_t const XMin = xmin;
-	size_t const XMax = xmax;
-	size_t const YMin = ymin;
-	size_t const YMax = ymax;
-	size_t const pitch = GameState->Pitch;
+	uint32 const XMin = RoundReal32ToUInt32(xmin);
+	uint32 const XMax = RoundReal32ToUInt32(xmax);
+	uint32 const YMin = RoundReal32ToUInt32(ymin);
+	uint32 const YMax = RoundReal32ToUInt32(ymax);
+	uint64 const pitch = GameState->Pitch;
 	framebuffer += (YMin * pitch);
-	for (size_t y = YMin; y != YMax; ++y) {
+	for (uint32 y = YMin; y != YMax; ++y) {
 		int *rect = (int*) framebuffer;
-		for (size_t x = XMin; x != XMax; ++x) {
+		for (uint32 x = XMin; x != XMax; ++x) {
 			rect[x] = 0;
 		}
 		framebuffer += pitch;
@@ -127,11 +136,11 @@ void GameUpdate(
 		);
 	}
 
-	int const xmin = 256;
-	int const ymin = 256;
-	int const RectangleWidth = 64;
-	int const RectangleHeight = 64;
-	int const xmax = xmin + RectangleWidth;
-	int const ymax = ymin + RectangleHeight;
+	real32 const xmin = 256;
+	real32 const ymin = 256;
+	real32 const RectangleWidth = 64;
+	real32 const RectangleHeight = 64;
+	real32 const xmax = xmin + RectangleWidth;
+	real32 const ymax = ymin + RectangleHeight;
 	DrawRectangle(Memory, xmin, xmax, ymin, ymax);
 }
